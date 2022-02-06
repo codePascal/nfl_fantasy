8c2d1d73d39ef7ee0b4cc6cfa440a1d5ef07aeda
pascal <mup0117@gmail.com>
2022-02-06 16:58:57 +0100

    clean up and minor changes in structure.

--- FFDP_course/Correlation_matrices/correlation_matrices.py
+++ FFDP_course/Correlation_matrices/correlation_matrices.py
@@ -7,59 +7,65 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import utils.data_handling as dh
+
 sns.set_style("whitegrid")
 
-pos_map = {
-    "HB": "RB",
-    "WR/RS": "WR",
-    "WR/PS": "WR",
-    "FB/TE": "TE",
-    "FB/HB": "RB"
-}
+YEAR = 2019
+SELECTOR = "PPRFantasyPoints"
 
 position_spots = {
     "QB": 1,
     "RB": 2,
     "WR": 3,
-    "TE": 2
+    "TE": 2,
 }
-
 positions = ["QB", "RB", "WR", "TE"]
-columns = ["Player", "Tm", "Pos", "Week", "PPRFantasyPoints"]
+features = ["Player", "Tm", "Pos", "Week", SELECTOR]
 
 
 def get_top_n_player_at_each_pos(data, pos, n):
+    """
+    Returns the n-best players on a position of a team.
+
+    :param data: averaged fantasy points per player
+    :type data: pandas.DataFrame
+    :param pos: position to get n-best players
+    :type pos: str
+    :param n: number of the top players on position
+    :type n: int
+    :return: top n players per team on defined position
+    :rtype: pandas.DataFrame
+    """
     data = data.loc[data["Pos"] == pos]
-    return data.groupby("Tm", as_index=False).apply(lambda x: x.nlargest(n, ["PPRFantasyPoints"]).min())
+    return data.groupby("Tm", as_index=False).apply(lambda x: x.nlargest(n, [SELECTOR]).min())
 
 
-df = pd.DataFrame()
-url = "https://raw.githubusercontent.com/fantasydatapros/data/master/weekly/{year}/week{week}.csv"
-year = 2019
-for week in range(1, 18):
-    weekly_df = pd.read_csv(url.format(year=year, week=week))
-    weekly_df["Week"] = week
-    df = pd.concat([df, weekly_df])
+# load accumulated weekly stats
+df = dh.concat_weekly_stats(YEAR)
 
-df = df.replace({"Pos": pos_map})
+# replace team names with standard abbreviations
+df = dh.replace_team_names(df)
+
+# limit to offensive players
 df = df.loc[df["Pos"].isin(positions)]
 
-new_df = df[columns]
-new_df = new_df.groupby(["Player", "Tm", "Pos"], as_index=False).agg({"PPRFantasyPoints": np.mean})
+# accumulate average of points for each player
+df = df[features].groupby(["Player", "Tm", "Pos"], as_index=False).agg({SELECTOR: np.mean})
 
+# get scored points per spot
 corr_df = pd.DataFrame()
 for pos, n_spots in position_spots.items():
     for n in range(1, n_spots + 1):
-        pos_df = get_top_n_player_at_each_pos(new_df, pos, n)
-        pos_df = pos_df.rename({"PPRFantasyPoints": f"{pos}{n}"}, axis=1)
+        pos_df = get_top_n_player_at_each_pos(df, pos, n)
+        pos_df = pos_df.rename({SELECTOR: f"{pos}{n}"}, axis=1)
         corr_df = pd.concat([corr_df, pos_df], axis=1)
 
+# clean up data
 corr_df = corr_df.dropna()
 corr_df = corr_df.drop(["Pos", "Player", "Tm"], axis=1)
 
+# plot the correlation heatmap
 plt.figure(figsize=(10, 7))
 sns.heatmap(corr_df.corr(), annot=True, cmap=sns.diverging_palette(0, 250))
-plt.savefig("plots/heatmap_pprfantasypoints_2019.png")
-
-
-
+plt.savefig("plots/heatmap_{selector}_{year}.png".format(selector=SELECTOR, year=YEAR))
