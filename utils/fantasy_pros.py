559cc683e1c1e258b9e3097fe21caed181308e65
pascal <mup0117@gmail.com>
2022-02-16 15:31:42 +0100

    Added preprocessing of stats to include stats, snapcount stats and schedule for each player over a whole season.

--- utils/fantasy_pros.py
+++ utils/fantasy_pros.py
@@ -3,8 +3,10 @@ Implements functions specifically designed for the data presented by
 FANTASYPROS. It is assumed that the data is ordered regarding fantasy
 points made during a season (FPTS).
 """
+import numpy as np
+import pandas as pd
 
-map_type_def = {
+map_type_dst = {
     "rank": int,
     "player": str,
     "defense_sacks": int,
@@ -14,18 +16,18 @@ map_type_def = {
     "defense_td": int,
     "defense_safety": int,
     "defense_spc_td": int,
-    "misc_g": int,
-    "misc_fpts": float,
-    "misc_fptsg": float,
-    "misc_rost": float,
+    "games": int,
+    "fantasy_points": float,
+    "fantasy_points_per_game": float,
+    "rost": float,
     "team": str
 }
 
 map_type_k = {
     "rank": int,
     "player": str,
-    "fg": int,
-    "fga": int,
+    "field_goal": int,
+    "field_goal_att": int,
     "pct": float,
     "lg": int,
     "1-19": int,
@@ -35,10 +37,10 @@ map_type_k = {
     "50+": int,
     "XPT": int,
     "XPA": int,
-    "misc_g": int,
-    "misc_fpts": float,
-    "misc_fptsg": float,
-    "misc_rost": float,
+    "games": int,
+    "fantasy_points": float,
+    "fantasy_points_per_game": float,
+    "rost": float,
     "team": str
 }
 
@@ -56,11 +58,11 @@ map_type_qb = {
     "rushing_att": int,
     "rushing_yds": int,
     "rushing_td": int,
-    "misc_fl": int,
-    "misc_g": int,
-    "misc_fpts": float,
-    "misc_fptsg": float,
-    "misc_rost": float,
+    "fumbles": int,
+    "games": int,
+    "fantasy_points": float,
+    "fantasy_points_per_game": float,
+    "rost": float,
     "team": str
 }
 
@@ -78,11 +80,11 @@ map_type_rb = {
     "receiving_yds": int,
     "receiving_yr": float,
     "receiving_td": int,
-    "misc_fl": int,
-    "misc_g": int,
-    "misc_fpts": float,
-    "misc_fptsg": float,
-    "misc_rost": float,
+    "fumbles": int,
+    "games": int,
+    "fantasy_points": float,
+    "fantasy_points_per_game": float,
+    "rost": float,
     "team": str
 }
 
@@ -99,11 +101,11 @@ map_type_te = {
     "rushing_att": int,
     "rushing_yds": int,
     "rushing_td": int,
-    "misc_fl": int,
-    "misc_g": int,
-    "misc_fpts": float,
-    "misc_fptsg": float,
-    "misc_rost": float,
+    "fumbles": int,
+    "games": int,
+    "fantasy_points": float,
+    "fantasy_points_per_game": float,
+    "rost": float,
     "team": str
 }
 
@@ -120,16 +122,113 @@ map_type_wr = {
     "rushing_att": int,
     "rushing_yds": int,
     "rushing_td": int,
-    "misc_fl": int,
-    "misc_g": int,
-    "misc_fpts": float,
-    "misc_fptsg": float,
-    "misc_rost": float,
+    "fumbles": int,
+    "games": int,
+    "fantasy_points": float,
+    "fantasy_points_per_game": float,
+    "rost": float,
     "team": str
 }
 
+map_type_snapcount = {
+    "player": str,
+    "position": str,
+    "team": str,
+    "games": int,
+    "snaps": int,
+    "snaps_per_game": int,
+    "snaps_percent": int,
+    "rush_percent": int,
+    "tgt_percent": int,
+    "touch_percent": int,
+    "util_percent": int,
+    "fantasy_points": float,
+    "points_per_100_snaps": float
+}
 
-def clean_stats_def(df):
+
+def concat_weekly_offensive_stats(year, weeks):
+    """
+    Concatenates weekly offensive stats. Takes only players into
+    account that have played that week.
+
+    :param year: year to evaluate
+    :type year: int
+    :param weeks: number of weeks of the season
+    :type weeks: int
+    :return: summarized weekly stats for season
+    :rtype: pandas.DataFrame
+    """
+    weekly_stats = pd.DataFrame()
+
+    for position in ["QB", "RB", "WR", "TE", "K"]:
+        for week in range(1, weeks + 1):
+            df = pd.read_csv(f"../data/weekly_stats/{year}/{position}/week_{week}.csv")
+            df.drop(df.columns[0], axis=1, inplace=True)
+            df = clean_stats(df, position)
+            df["week"] = week
+            df["position"] = position
+
+            # rank is not required
+            df.drop("rank", axis=1, inplace=True)
+
+            weekly_stats = pd.concat([weekly_stats, df])
+
+    weekly_stats.reset_index(drop=True, inplace=True)
+    return weekly_stats
+
+
+def concat_weekly_snapcounts(year, weeks):
+    """
+    Concatenates weekly snapcount stats.
+
+    :param year: year to evaluate
+    :type year: int
+    :param weeks: number of weeks of the season
+    :type weeks: int
+    :return: summarized snapcount analysis for the season
+    :rtype: pandas.DataFrame
+    """
+    snapcount_stats = pd.DataFrame()
+
+    for week in range(1, weeks + 1):
+        df = pd.read_csv(f"../data/weekly_snapcounts/{year}/week_{week}.csv")
+        df.drop(df.columns[0], axis=1, inplace=True)
+        df = clean_snapcount_analysis(df)
+        df["week"] = week
+        snapcount_stats = pd.concat([snapcount_stats, df])
+
+    snapcount_stats.reset_index(drop=True, inplace=True)
+    return snapcount_stats
+
+
+def clean_stats(df, position):
+    """
+    Clean the stats found here:
+    https://www.fantasypros.com/nfl/stats/
+
+    :param df: data loaded from csv
+    :type df: pandas.DataFrame
+    :param position: position to clean
+    :type position: str
+    :return: cleaned data
+    :rtype: pandas.DataFrame
+    """
+    if position == "DST":
+        return clean_stats_dst(df)
+    elif position == "K":
+        return clean_stats_k(df)
+    elif position == "QB":
+        return clean_stats_qb(df)
+    elif position == "RB":
+        return clean_stats_rb(df)
+    elif position == "TE":
+        return clean_stats_te(df)
+    elif position == "WR":
+        return clean_stats_wr(df)
+
+
+def clean_stats_dst(df):
     """
     Cleans the defensive statistics found here:
     https://www.fantasypros.com/nfl/stats/dst.php
@@ -140,15 +239,15 @@ def clean_stats_def(df):
     :rtype: pandas.DataFrame
     """
     # rename column names in a more descriptive manner
-    df.columns = list(map_type_def.keys())[:-1]
+    df.columns = list(map_type_dst.keys())[:-1]
 
     # transform column entries
     df["team"] = df.apply(get_team, axis=1)
     df["player"] = df["player"].apply(transform_name)
-    df["misc_rost"] = df["misc_rost"].apply(transform_rost)
+    df["rost"] = df["rost"].apply(transform_rost)
 
     # set types
-    df = df.astype(map_type_def)
+    df = df.astype(map_type_dst)
 
     return df
 
@@ -169,7 +268,7 @@ def clean_stats_k(df):
     # transform column entries
     df["team"] = df.apply(get_team, axis=1)
     df["player"] = df["player"].apply(transform_name)
-    df["misc_rost"] = df["misc_rost"].apply(transform_rost)
+    df["rost"] = df["rost"].apply(transform_rost)
 
     # set types
     df = df.astype(map_type_k)
@@ -195,7 +294,7 @@ def clean_stats_qb(df):
     df["player"] = df["player"].apply(transform_name)
     df["passing_yds"] = df["passing_yds"].apply(transform_yards)
     df["rushing_yds"] = df["rushing_yds"].apply(transform_yards)
-    df["misc_rost"] = df["misc_rost"].apply(transform_rost)
+    df["rost"] = df["rost"].apply(transform_rost)
 
     # set types
     df = df.astype(map_type_qb)
@@ -221,7 +320,7 @@ def clean_stats_rb(df):
     df["player"] = df["player"].apply(transform_name)
     df["rushing_yds"] = df["rushing_yds"].apply(transform_yards)
     df["receiving_yds"] = df["receiving_yds"].apply(transform_yards)
-    df["misc_rost"] = df["misc_rost"].apply(transform_rost)
+    df["rost"] = df["rost"].apply(transform_rost)
 
     # set types
     df = df.astype(map_type_rb)
@@ -247,7 +346,7 @@ def clean_stats_te(df):
     df["player"] = df["player"].apply(transform_name)
     df["receiving_yds"] = df["receiving_yds"].apply(transform_yards)
     df["rushing_yds"] = df["rushing_yds"].apply(transform_yards)
-    df["misc_rost"] = df["misc_rost"].apply(transform_rost)
+    df["rost"] = df["rost"].apply(transform_rost)
 
     # set types
     df = df.astype(map_type_te)
@@ -273,7 +372,7 @@ def clean_stats_wr(df):
     df["player"] = df["player"].apply(transform_name)
     df["receiving_yds"] = df["receiving_yds"].apply(transform_yards)
     df["rushing_yds"] = df["rushing_yds"].apply(transform_yards)
-    df["misc_rost"] = df["misc_rost"].apply(transform_rost)
+    df["rost"] = df["rost"].apply(transform_rost)
 
     # set types
     df = df.astype(map_type_wr)
@@ -290,7 +389,7 @@ def get_team(player):
     :return: players team
     :rtype: str
     """
-    return player["player"].split()[-1][1:-1]
+    return player["player"].split('(')[1].split(')')[0]
 
 
 def transform_name(name):
@@ -302,9 +401,7 @@ def transform_name(name):
     :return: player name only
     :rtype: str
     """
-    first_name = name.split()[0]
-    last_name = name.split()[1]
-    return " ".join([first_name, last_name])
+    return name.split('(')[0]
 
 
 def transform_yards(yards):
@@ -336,49 +433,78 @@ def transform_rost(rost):
 
 def clean_schedule(df):
     """
-    Cleans up schedule and sets team as index.
+    Cleans up schedule and returns the team and its opponent per
+    week. Includes if at home or away.
 
     :param df: schedule as read from csv
     :type df: pandas.DataFrame
     :return: clean schedule
     :rtype: pandas.DataFrame
     """
+    # prepare schedule first
     df.columns = ["team"] + [str(i) for i in range(1, df.shape[1])]
     df.set_index("team", drop=True, inplace=True)
-    return df
+    df.dropna(inplace=True)
+
+    # extract game information
+    schedule = pd.DataFrame(columns=["team", "opponent", "week", "home"])
+    for i, games in df.iterrows():
+        for j, game in enumerate(games):
+            schedule = pd.concat([schedule, pd.DataFrame({"team": [i],
+                                                          "opponent": [get_opponent(game)],
+                                                          "week": [j + 1],
+                                                          "home": [get_location(game)]})],
+                                 ignore_index=True)
+
+    return schedule
 
 
-def get_opponent(df, team, week):
+def get_opponent(game):
     """
-    Returns the opponent.
-    :param df: schedule
-    :type df: pandas.DataFrame
-    :param team: abbreviation of team
-    :type team: str
-    :param week: week to get opponent for
-    :type week: int
-    :return: opponent
+    Returns the opponent of the game on the view of the team. If the
+    team has bye week, returns BYE.
+
+    :param game: information about location and opponent
+    :type game: str
+    :return: opponent or BYE
     :rtype: str
     """
-    return df.loc[team, str(week)].split()[1]
+    if game == "BYE":
+        return game
+    return game.split()[1]
 
 
-def get_place(df, team, week):
+def get_location(game):
     """
-    Returns if the team has played home.
+    Returns the location of the game on the view of the team. If the
+    team has bye week, returns nan.
 
-    :param df: cleaned schedule
-    :type df: pandas.DataFrame
-    :param team: abbreviation of team
-    :type team: str
-    :param week: week to get location for
-    :type week: int
-    :return: true if home, false if away
+    :param game: information about location and opponent
+    :type game: str
+    :return: location, home = True, away = False
     :rtype: bool
     """
-    loc = df.loc[team, str(week)].split()[0]
-    if loc == "@":
+    if game.startswith('@'):
         return False
-    else:
+    elif game.startswith("vs"):
         return True
+    else:
+        return np.nan
 
+
+def clean_snapcount_analysis(df):
+    """
+    Cleans up snapcount analysis.
+
+    :param df: analysis as read from csv
+    :type df: pandas.DataFrame
+    :return: cleaned snapcount analysis
+    :rtype: pandas.DataFrame
+    """
+    # rename column names in a more descriptive manner
+    df.columns = list(map_type_snapcount.keys())
+
+    # set types
+    df = df.astype(map_type_snapcount)
+
+    return df
