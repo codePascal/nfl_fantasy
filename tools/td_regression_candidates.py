7b69c74eee053f9921f19ae93708b935f19be760
pascal <mup0117@gmail.com>
2022-02-18 12:56:32 +0100

    Clean up and added parser to td regression script; updated to latest season.

--- tools/td_regression_candidates.py
+++ tools/td_regression_candidates.py
@@ -3,25 +3,15 @@ Evaluates the TD regression candidates for a given position regarding
 the mean. A high or low touchdown total is not predictive for future
 performance as these players will often regress towards the mean or
 average the following year.
-
-Play-by-play source season 2009-2018:
-    https://www.kaggle.com/maxhorowitz/nflplaybyplay2009to2016?select=NFL+Play+by+Play+2009-2018+%28v5%29.csv
 """
+import argparse
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
-import numpy as np
-
-import utils.fantasy_pros as fp
+import tqdm
 
 sns.set_style("whitegrid")
-
-PREPROCESSED_BASIS = True
-PREPROCESSED_EST = True
-PREPROCESSED_ACTUAL = False
-
-PLAY = "rec"  # rush, pass, rec
-POSITION = "RB"  # QB, RB, TE, WR
 
 
 def fix_player_names(name):
@@ -32,75 +22,74 @@ def fix_player_names(name):
 
 
 if __name__ == "__main__":
+    # command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year", type=int)
+    parser.add_argument("play", type=str)
+    parser.add_argument("position", type=str)
+    args = parser.parse_args()
+
+    # status
+    print(f"TD regression analysis for {args.play} {args.position} in season {args.year}.")
+
     # calculate probability of scoring a touchdown depending on
     # distance to endzone based on historical play-by-play raw
-    if not PREPROCESSED_BASIS:
-        # load in single chunks
-        chunks = pd.read_csv("../raw/play-by-play/pbp_2009to2018.csv", iterator=True, low_memory=False,
-                             chunksize=10000)
+    chunks = pd.read_csv("../preprocessed/play-by-play/pbp_1999to2021.csv", iterator=True, low_memory=False,
+                         chunksize=10000)
 
-        # concat to dataframe
-        df = pd.DataFrame()
-        for chunk in chunks:
-            df = pd.concat([df, chunk])
+    # concat only necessary columns to dataframe
+    df_prob = pd.DataFrame()
+    for chunk in tqdm.tqdm(chunks):
+        chunk = chunk.loc[:, ["rush_attempt", "rush_touchdown", "pass_attempt", "pass_touchdown", "yardline_100",
+                              "two_point_attempt", "year"]]
+        df_prob = pd.concat([df_prob, chunk])
 
-        # select pass or run attempts
-        if PLAY == "pass" or PLAY == "rec":
-            # passing/receiving: pass attempt that ended in a touchdown
-            df = df.loc[:, ["pass_attempt", "pass_touchdown", "yardline_100", "two_point_attempt"]]
-            df = df.rename({"pass_attempt": "attempt", "pass_touchdown": "touchdown"}, axis=1)
-        elif PLAY == "rush":
-            # rushing: rush attempt that ended in a touchdown
-            df = df.loc[:, ["rush_attempt", "rush_touchdown", "yardline_100", "two_point_attempt"]]
-            df = df.rename({"rush_attempt": "attempt", "rush_touchdown": "touchdown"}, axis=1)
+    # select data from previous years
+    df_prob = df_prob.loc[(df_prob["year"] < args.year)]
 
-        # select only normal attempts
-        df = df.loc[(df["two_point_attempt"] == 0) & (df["attempt"] == 1)]
+    # select pass or run attempts
+    if args.play == "pass" or args.play == "rec":
+        # passing/receiving: pass attempt that ended in a touchdown
+        df_prob = df_prob.loc[:, ["pass_attempt", "pass_touchdown", "yardline_100", "two_point_attempt"]]
+        df_prob = df_prob.rename({"pass_attempt": "attempt", "pass_touchdown": "touchdown"}, axis=1)
+    elif args.play == "rush":
+        # rushing: rush attempt that ended in a touchdown
+        df_prob = df_prob.loc[:, ["rush_attempt", "rush_touchdown", "yardline_100", "two_point_attempt"]]
+        df_prob = df_prob.rename({"rush_attempt": "attempt", "rush_touchdown": "touchdown"}, axis=1)
 
-        # find probability of scoring a touchdown depending on distance to endzone
-        df_prob = df.groupby("yardline_100")["touchdown"].value_counts(normalize=True)
-        df_prob = pd.DataFrame({"probability_of_touchdown": df_prob.values}, index=df_prob.index).reset_index()
+    # select only normal attempts
+    df_prob = df_prob.loc[(df_prob["two_point_attempt"] == 0) & (df_prob["attempt"] == 1)]
 
-        # keep stats where touchdown was scored and drop column
-        df_prob = df_prob.loc[(df_prob["touchdown"] == 1)]
-        df_prob = df_prob.drop("touchdown", axis=1)
+    # find probability of scoring a touchdown depending on distance to endzone
+    df_prob = df_prob.groupby("yardline_100")["touchdown"].value_counts(normalize=True)
+    df_prob = pd.DataFrame({"probability_of_touchdown": df_prob.values}, index=df_prob.index).reset_index()
 
-        # save preprocessed probability raw
-        df_prob.to_csv(f"../preprocessed/td_regression/preprocessed_probability_{PLAY}.csv", index=False)
-    else:
-        # load preprocessed probability raw
-        df_prob = pd.read_csv(f"../preprocessed/td_regression/preprocessed_probability_{PLAY}.csv")
+    # keep stats where touchdown was scored and drop column
+    df_prob = df_prob.loc[(df_prob["touchdown"] == 1)]
+    df_prob = df_prob.drop("touchdown", axis=1)
 
     # plot probability of scoring a touchdown
-    df_prob.plot(x="yardline_100", y="probability_of_touchdown", title=f"Probability for {PLAY}ing TD")
-    plt.savefig(f"../plots/td_regression_candidates/touchdown_probability_{PLAY}.png")
+    df_prob.plot(x="yardline_100", y="probability_of_touchdown", title=f"Probability for {args.play}ing TD")
+    plt.savefig(f"../reports/td_regression_candidates/touchdown_probability_{args.play}.png")
 
-    if not PREPROCESSED_EST:
-        # load 2019 play-by-play raw
-        chunks = pd.read_csv("../raw/play-by-play/play_by_play_2019.csv", iterator=True, low_memory=False, chunksize=10000,
-                             index_col=0)
-        df = pd.DataFrame()
-        for chunk in chunks:
-            df = pd.concat([df, chunk])
+    # load latest play-by-play data
+    chunks = pd.read_csv(f"../raw/play-by-play/play_by_play_{args.year}.csv", iterator=True, low_memory=False,
+                         chunksize=10000, index_col=0)
+    df_train = pd.DataFrame()
+    for chunk in tqdm.tqdm(chunks):
+        df_train = pd.concat([df_train, chunk])
 
-        # get player, team and distance to endzone
-        df_train = pd.DataFrame()
-        if PLAY == "pass":
-            df_train = df.loc[:, ["passer_player_name", "posteam", "yardline_100"]]
-        elif PLAY == "rec":
-            df_train = df.loc[:, ["receiver_player_name", "posteam", "yardline_100"]]
-        elif PLAY == "rush":
-            df_train = df.loc[:, ["rusher_player_name", "posteam", "yardline_100"]]
-        df_train = df_train.dropna()
+    # get player, team and distance to endzone
+    if args.play == "pass":
+        df_train = df_train.loc[:, ["passer_player_name", "posteam", "yardline_100"]]
+    elif args.play == "rec":
+        df_train = df_train.loc[:, ["receiver_player_name", "posteam", "yardline_100"]]
+    elif args.play == "rush":
+        df_train = df_train.loc[:, ["rusher_player_name", "posteam", "yardline_100"]]
+    df_train = df_train.dropna()
 
-        # rename features
-        df_train = df_train.rename(columns={df_train.columns[0]: "player", "posteam": "team"})
-
-        # save preprocessed training raw
-        df_train.to_csv(f"../preprocessed/td_regression/preprocessed_training_{PLAY}.csv", index=False)
-    else:
-        # load preprocessed training raw
-        df_train = pd.read_csv(f"../preprocessed/td_regression/preprocessed_training_{PLAY}.csv")
+    # rename features
+    df_train = df_train.rename(columns={df_train.columns[0]: "player", "posteam": "team"})
 
     # join probability with training
     data = df_train.merge(df_prob, how="left", on="yardline_100")
@@ -113,36 +102,32 @@ if __name__ == "__main__":
     # rank the expected touchdowns
     data["Expected touchdowns rank"] = data["Expected touchdowns"].rank(ascending=False)
 
-    if not PREPROCESSED_ACTUAL:
-        # load final stats
-        chunks = pd.read_csv(f"../raw/yearly_stats/{POSITION}/{POSITION}_2019.csv", iterator=True, low_memory=False,
-                             chunksize=10000)
-        df = pd.DataFrame()
-        for chunk in chunks:
-            df = pd.concat([df, chunk])
-        df = df.dropna()
+    # load final stats
+    chunks = pd.read_csv(f"../preprocessed/stats/offense_summary_{args.year}.csv", iterator=True, low_memory=False,
+                         chunksize=10000)
+    df = pd.DataFrame()
+    for chunk in tqdm.tqdm(chunks):
+        df = pd.concat([df, chunk])
 
-        # clean stats for defined position
-        df_actual = fp.clean_stats(df, POSITION)
+    # get only defined position
+    df_actual = df.loc[(df["position"] == args.position)]
 
-        # get player, team and td
-        if PLAY == "pass":
-            df_actual = df_actual.loc[:, ["player", "team", "passing_td"]]
-            df_actual = df_actual.rename({"passing_td": "touchdowns"}, axis=1)
-        elif PLAY == "rec":
-            df_actual = df_actual.loc[:, ["player", "team", "receiving_td"]]
-            df_actual = df_actual.rename({"receiving_td": "touchdowns"}, axis=1)
-        elif PLAY == "rush":
-            df_actual = df_actual.loc[:, ["player", "team", "rushing_td"]]
-            df_actual = df_actual.rename({"rushing_td": "touchdowns"}, axis=1)
+    # get player, team and td
+    if args.play == "pass":
+        df_actual = df_actual.loc[:, ["player", "team", "passing_td"]]
+        df_actual = df_actual.rename({"passing_td": "touchdowns"}, axis=1)
+    elif args.play == "rec":
+        df_actual = df_actual.loc[:, ["player", "team", "receiving_td"]]
+        df_actual = df_actual.rename({"receiving_td": "touchdowns"}, axis=1)
+    elif args.play == "rush":
+        df_actual = df_actual.loc[:, ["player", "team", "rushing_td"]]
+        df_actual = df_actual.rename({"rushing_td": "touchdowns"}, axis=1)
 
-        # clean up player names
-        df_actual["player"] = df_actual["player"].apply(fix_player_names)
+    # clean up player names
+    df_actual["player"] = df_actual["player"].apply(fix_player_names)
 
-        # save preprocessed player raw
-        df_actual.to_csv(f"../preprocessed/td_regression/preprocessed_actual_{PLAY}_{POSITION}.csv", index=False)
-    else:
-        df_actual = pd.read_csv(f"../preprocessed/td_regression/preprocessed_actual_{PLAY}_{POSITION}.csv")
+    # summarize the year
+    df_actual = df_actual.groupby(["player", "team"]).agg({"touchdowns": np.sum}).reset_index()
 
     # merge raw and drop position
     data = df_actual.merge(data, how="left", on=["player", "team"]).dropna()
@@ -160,7 +145,7 @@ if __name__ == "__main__":
 
     # explore the statistics
     fig, ax = plt.subplots(figsize=(12, 8))
-    ax.set_title(f"Positive regression candidates for {PLAY} {POSITION}")
+    ax.set_title(f"Positive regression candidates for {args.play} {args.position} in season {args.year}")
 
     # plot expected vs actual touchdowns and incorporate regression candidate values
     sns.scatterplot(
@@ -180,7 +165,7 @@ if __name__ == "__main__":
     # show top five negative and positive regression candidates
     data_sorted = data.sort_values(by="Regression candidate", ascending=False).reset_index()
     for i, row in data_sorted.iterrows():
-        if i <= 4 or i >= len(data_sorted) - 1 - 4:
+        if i <= 1 or i >= len(data_sorted) - 1 - 1:
             ax.text(
                 x=row["Expected touchdowns"] + 0.1,
                 y=row["Actual touchdowns"] + 0.05,
@@ -188,4 +173,10 @@ if __name__ == "__main__":
             )
 
     # save
-    plt.savefig(f"../plots/td_regression_candidates/regression_candidates_{PLAY}_{POSITION}.png")
+    data_sorted.to_csv(
+        f"../reports/td_regression_candidates/td_regression_candidates_{args.play}_{args.position}_{args.year}.csv",
+        index=False
+    )
+    plt.savefig(
+        f"../reports/td_regression_candidates/regression_candidates_{args.play}_{args.position}_{args.year}.png"
+    )
