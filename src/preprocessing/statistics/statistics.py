31ecce06d06a7b4ca6c7a4845c54ece92fc19ad5
pascal <mup0117@gmail.com>
2022-02-27 15:25:52 +0100

    Updated statistics accumulation to gather stats, snapcounts and schedule for active players, for each position separate.

---
+++ src/preprocessing/statistics/statistics.py
@@ -0,0 +1,72 @@
+"""
+Accumulates weekly stats with snapcounts and schedule for a season.
+
+Running this script will store the accumulated statistics for a given
+year range.
+"""
+import numpy as np
+import pandas as pd
+
+from abc import ABC
+
+from config.mapping import teams, team_changes_map
+from src.loader.fantasypros.schedule import Schedule
+from src.preprocessing.preprocessing import Preprocessing
+from src.preprocessing.statistics.snapcounts import Snapcounts
+from src.preprocessing.statistics.stats import Stats
+
+
+class Statistics(Preprocessing, ABC):
+    def __init__(self, position, year, refresh=False):
+        Preprocessing.__init__(self, year, refresh)
+        self.position = position
+        self.year = year
+
+        self.filename = f"statistics_{self.year}.csv"
+        self.dir = f"../preprocessed/statistics"
+
+    def concat_data(self):
+        """ Concatenates weekly stats, snapcounts and schedule
+        for given position. """
+        # get snapcounts only for position
+        snapcounts = Snapcounts(self.year).get_accumulated_data()
+        snapcounts = snapcounts.loc[snapcounts["position"] == self.position]
+
+        # merge with weekly accumulated stats
+        stats = pd.merge(snapcounts,
+                         Stats(self.position, self.year).get_accumulated_data(),
+                         how="inner",
+                         on=["player", "week", "year", "fantasy_points", "games", "position", "team"])
+
+        # drop not relevant columns
+        stats.drop(["rank", "rost", "fantasy_points_per_game", "snaps_per_game"], axis=1, inplace=True)
+
+        # clean up teams to latest names
+        stats["team"] = stats["team"].apply(fix_teams)
+
+        # merge schedule
+        stats = pd.merge(stats, Schedule(self.year).get_data(), how="inner", on=["team", "week", "year"])
+
+        return stats
+
+
+def fix_teams(team):
+    """ Updates team abbreviations to the latest ones. """
+    if team not in teams and team is not np.nan:
+        if team in team_changes_map.keys():
+            return team_changes_map[team]
+        elif team != "FA":
+            print("Team abbreviation", team, "not available.")
+    return team
+
+
+def store_all():
+    """ Accumulates and stores the accumulated statistics. """
+    years = (2010, 2021)
+    for position in ["QB", "RB", "TE", "WR"]:
+        for year in range(years[0], years[1] + 1):
+            Statistics(position, year).store_accumulated_data()
+
+
+if __name__ == "__main__":
+    store_all()
