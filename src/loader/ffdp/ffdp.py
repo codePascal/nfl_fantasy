d3fa56eb49a096c8f2cbe573b2979d563c36c645
pascal <mup0117@gmail.com>
2022-02-27 22:26:29 +0100

    Updated adding players.

--- src/loader/ffdp/ffdp.py
+++ src/loader/ffdp/ffdp.py
@@ -5,11 +5,15 @@ players name, the year and his team for that season.
 
 Note: it currently just assigns teams at is and without notice when
 players switched teams during the season.
+
+This script should only be run to generate the assignments and then
+use these.
 """
 import os
 import pandas as pd
 
 from config.mapping import teams, team_changes_map
+from src.loader.fantasypros.stats import YearlyStats
 
 
 class FFDPLoader:
@@ -18,15 +22,15 @@ class FFDPLoader:
         self.filename = f"players_{self.year}.csv"
         self.dir = f"../raw/players"
 
-    def load_data(self):
-        return pd.read_csv(f"../../data/yearly/{self.year}.csv")
-
-    def clean_data(self):
+    def fetch_data(self):
         df = self.load_data()
         df = df.loc[:, ["Player", "Tm", "Pos"]]
         df["Tm"] = df["Tm"].apply(self.fix_team)
         df.columns = ["player", "team", "position"]
         return df
+
+    def load_data(self):
+        return pd.read_csv(f"../../data/yearly/{self.year}.csv")
 
     def fix_team(self, team):
         if team in teams:
@@ -44,15 +48,12 @@ class FFDPLoader:
                 return team
 
     def store_data(self):
-        self.clean_data().to_csv(os.path.join(self.dir, self.filename), index=False)
+        self.fetch_data().to_csv(os.path.join(self.dir, self.filename), index=False, header=True)
 
-
-def store_all():
-    for year in range(2009, 2020 + 1):
-        FFDPLoader(year).store_data()
-
-
-if __name__ == "__main__":
-    store_all()
-
-
+    def add_data(self):
+        df = pd.DataFrame()
+        for position in ["QB", "RB", "TE", "WR"]:
+            yearly = YearlyStats(position, self.year).get_data()
+            yearly = yearly.loc[:, ["player", "team", "position"]]
+            df = pd.concat([df, yearly])
+        df.to_csv(os.path.join(self.dir, self.filename), index=False, header=True)
