c8210de2fb6572cbbfc48bb7d294f8b68d068853
pascal <mup0117@gmail.com>
2022-02-27 16:57:14 +0100

    Updated ARIMA model and moved to src/models.

--- src/models/ARIMA.py
+++ src/models/ARIMA.py
@@ -31,7 +31,7 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 from pmdarima.arima import auto_arima
 
-from src.preprocessing.stats import Stats
+from src.preprocessing.statistics.statistics import Statistics
 
 SHOW_PLOT = True
 
@@ -99,44 +99,46 @@ def calculate_fantasy_points(series):
     pass_td = series["passing_td"]
     rush_yds = series["rushing_yds"]
     rush_td = series["rushing_td"]
-    fumbles_lost = series["fumbles_lost"]
+    fumbles_lost = series["lst"]
     return 1 / 25 * pass_yds + (-1) * pass_int + 4 * pass_td + 0.1 * rush_yds + 6 * rush_td + (-2) * fumbles_lost
 
 
-# define selector and player (here for QB)
-selectors = ["passing_yds", "passing_int", "passing_td", "rushing_yds", "rushing_td", "fumbles_lost"]
-player = "Aaron Rodgers"
-year_to_pred = 2021
+if __name__ == "__main__":
+    # define selector and player (here for QB)
+    selectors = ["passing_yds", "passing_int", "passing_td", "rushing_yds", "rushing_td", "lst"]
+    player = "Aaron Rodgers"
+    position = "QB"
+    year_to_pred = 2021
 
-# load weekly stats for given year range
-df = pd.DataFrame()
-for year in range(2010, year_to_pred + 1):
-    df = pd.concat([df, Stats(year).get_accumulated_data()])
+    # load weekly stats for given year range
+    df = pd.DataFrame()
+    for year in range(2010, year_to_pred + 1):
+        df = pd.concat([df, Statistics(position, year).get_accumulated_data()])
 
-# get stats where player played
-df_player = df.loc[(df["player"] == player) & (df["games"] != 0)].reset_index(drop=True)
+    # get stats where player played
+    df_player = df.loc[(df["player"] == player) & (df["games"] != 0)].reset_index(drop=True)
 
-# get prediction for each performance metric
-df_pred = pd.DataFrame(np.nan, index=selectors, columns=["actual", "predicted"])
-for selector in selectors:
-    if SHOW_PLOT:
-        # plot the selected data
-        df_player[selector].plot()
-        plt.show()
+    # get prediction for each performance metric
+    df_pred = pd.DataFrame(np.nan, index=selectors, columns=["actual", "predicted"])
+    for selector in selectors:
+        if SHOW_PLOT:
+            # plot the selected data
+            df_player[selector].plot()
+            plt.show()
 
-    # split to training and test
-    df_train = df_player.loc[df_player["year"] < year_to_pred, [selector]].reset_index(drop=True)
-    df_test = df_player.loc[df_player["year"] == year_to_pred, [selector]].reset_index(drop=True)
+        # split to training and test
+        df_train = df_player.loc[df_player["year"] < year_to_pred, [selector]].reset_index(drop=True)
+        df_test = df_player.loc[df_player["year"] == year_to_pred, [selector]].reset_index(drop=True)
 
-    # predict
-    # TODO add test if data is sufficient to predict
-    #  "ValueError: sample size is too short to use selected regression component"
-    #  > 32
-    print(len(df_train))
-    df_test[f"{selector}_pred"] = predict_performance(df_train, selector, df_test.shape[0])
-    df_pred.loc[selector, "actual"] = df_test[selector].sum()
-    df_pred.loc[selector, "predicted"] = df_test[f"{selector}_pred"].sum()
+        # predict
+        # TODO add test if data is sufficient to predict
+        #  "ValueError: sample size is too short to use selected regression component"
+        #  > 32
+        print(len(df_train))
+        df_test[f"{selector}_pred"] = predict_performance(df_train, selector, df_test.shape[0])
+        df_pred.loc[selector, "actual"] = df_test[selector].sum()
+        df_pred.loc[selector, "predicted"] = df_test[f"{selector}_pred"].sum()
 
-# calculate fantasy points
-print(calculate_fantasy_points(df_pred.loc[:, "actual"]))
-print(calculate_fantasy_points(df_pred.loc[:, "predicted"]))
+    # calculate fantasy points
+    print(calculate_fantasy_points(df_pred.loc[:, "actual"]))
+    print(calculate_fantasy_points(df_pred.loc[:, "predicted"]))
