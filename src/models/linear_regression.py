294366d0cb35da429106ce0e4579bdd82a067247
pascal <mup0117@gmail.com>
2022-03-02 16:57:00 +0100

    Initialized model class with linear regression child.

---
+++ src/models/linear_regression.py
@@ -0,0 +1,28 @@
+
+import pandas as pd
+
+from abc import ABC
+
+from config.mapping import week_map
+
+from src.models.models import Models
+from src.preprocessing.statistics.statistics import Statistics
+
+
+class LinReg(Models, ABC):
+    def __init__(self, position):
+        Models.__init__(self)
+        self.position = position
+
+    def run(self):
+        # load stats for position
+        df = pd.DataFrame()
+        for year in range(2016, 2022):
+            df = pd.concat([df, Statistics(self.position, year, refresh=True).get_accumulated_data()])
+        df.to_csv("test.csv")
+
+
+if __name__ == "__main__":
+    LinReg("QB").run()
+
+
---
+++ src/models/models.py
@@ -0,0 +1,8 @@
+
+
+class Models:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
