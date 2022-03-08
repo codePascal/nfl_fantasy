a2d46c0eaa459b70f4299cdf5f10c73e31169150
codePascal <mup0117@gmail.com>
2022-03-08 14:10:55 +0100

    Updated column names for defense and offense.

--- config/espn.py
+++ config/espn.py
@@ -6,7 +6,7 @@ https://www.hometeamsonline.com/teams/popups/Glossary.asp?s=football
 # maps the column names and types for defense passing
 defense_passing_map = {
     "team": str,
-    "games": float,
+    "games_defense": float,
     "passing_cmp_allowed": float,
     "passing_att_allowed": float,
     "passing_cmppct_allowed": float,
@@ -24,7 +24,7 @@ defense_passing_map = {
 # maps the column names and types for defense rushing
 defense_rushing_map = {
     "team": str,
-    "games": float,
+    "games_defense": float,
     "rushing_att_allowed": float,
     "rushing_yds_allowed": float,
     "rushing_avg_allowed": float,
@@ -38,7 +38,7 @@ defense_rushing_map = {
 # maps the column names and types for defense receiving
 defense_receiving_map = {
     "team": str,
-    "games": float,
+    "games_defense": float,
     "receiving_rec_allowed": float,
     "receiving_yds_allowed": float,
     "receiving_avg_allowed": float,
@@ -52,7 +52,7 @@ defense_receiving_map = {
 # maps the column names and types for defense downs
 defense_downs_map = {
     "team": str,
-    "games": float,
+    "games_defense": float,
     "1st_total_allowed": float,
     "1st_rush_allowed": float,
     "1st_pass_allowed": float,
@@ -70,63 +70,63 @@ defense_downs_map = {
 # maps the column names and types for offense passing
 offense_passing_map = {
     "team": str,
-    "games": float,
-    "passing_cmp": float,
-    "passing_att": float,
-    "passing_cmppct": float,
-    "passing_yds": float,
-    "passing_avg": float,
-    "passing_yds_per_game": float,
-    "passing_longest": float,
-    "passing_td": float,
-    "passing_int": float,
-    "passing_sack": float,
-    "sack_yards_lost": float,
-    "passer_rating": float
+    "games_offense": float,
+    "passing_cmp_offense": float,
+    "passing_att_offense": float,
+    "passing_cmppct_offense": float,
+    "passing_yds_offense": float,
+    "passing_avg_offense": float,
+    "passing_yds_per_game_offense": float,
+    "passing_longest_offense": float,
+    "passing_td_offense": float,
+    "passing_int_offense": float,
+    "passing_sack_offense": float,
+    "sack_yards_lost_offense": float,
+    "passer_rating_offense": float
 }
 
 # maps the column names and types for offense rushing
 offense_rushing_map = {
     "team": str,
-    "games": float,
-    "rushing_att": float,
-    "rushing_yds": float,
-    "rushing_avg": float,
-    "rushing_yds_per_game": float,
-    "rushing_longest": float,
-    "rushing_td": float,
-    "rushing_fumbles": float,
-    "rushing_fumbles_lost": float
+    "games_offense": float,
+    "rushing_att_offense": float,
+    "rushing_yds_offense": float,
+    "rushing_avg_offense": float,
+    "rushing_yds_per_game_offense": float,
+    "rushing_longest_offense": float,
+    "rushing_td_offense": float,
+    "rushing_fumbles_offense": float,
+    "rushing_fumbles_lost_offense": float
 }
 
 # maps the column names and types for offense receiving
 offense_receiving_map = {
     "team": str,
-    "games": float,
-    "receiving_rec": float,
-    "receiving_yds": float,
-    "receiving_avg": float,
-    "receiving_yds_per_game": float,
-    "receiving_longest": float,
-    "receiving_td": float,
-    "receiving_fumbles": float,
-    "receiving_fumbles_lost": float
+    "games_offense": float,
+    "receiving_rec_offense": float,
+    "receiving_yds_offense": float,
+    "receiving_avg_offense": float,
+    "receiving_yds_per_game_offense": float,
+    "receiving_longest_offense": float,
+    "receiving_td_offense": float,
+    "receiving_fumbles_offense": float,
+    "receiving_fumbles_lost_offense": float
 }
 
 # maps the column names and types for defense downs
 offense_downs_map = {
     "team": str,
-    "games": float,
-    "1st_total": float,
-    "1st_rush": float,
-    "1st_pass": float,
-    "1st_penalty": float,
-    "3rd_conv": float,
-    "3rd_att": float,
-    "3rd_pct": float,
-    "4th_conv": float,
-    "4th_att": float,
-    "4th_pct": float,
+    "games_offense": float,
+    "1st_total_offense": float,
+    "1st_rush_offense": float,
+    "1st_pass_offense": float,
+    "1st_penalty_offense": float,
+    "3rd_conv_offense": float,
+    "3rd_att_offense": float,
+    "3rd_pct_offense": float,
+    "4th_conv_offense": float,
+    "4th_att_offense": float,
+    "4th_pct_offense": float,
     "offense_penalties": float,
     "offense_yds_lost": float
 }
